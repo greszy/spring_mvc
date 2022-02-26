a6850e2411befbf824d3adeddd7eb3f9d3fdd52e
Gregory Szymanski <gregoryszy@gmail.com>
2022-02-25 22:48:15 -0500

    Adds a DeferredResult implementation of async processing in the Search Controller.

--- src/main/java/com/test/hplus/controllers/SearchController.java
+++ src/main/java/com/test/hplus/controllers/SearchController.java
@@ -3,10 +3,12 @@ package com.test.hplus.controllers;
 import com.test.hplus.beans.Product;
 import com.test.hplus.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.task.AsyncTaskExecutor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.context.request.async.DeferredResult;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
@@ -19,27 +21,46 @@ public class SearchController {
     @Autowired
     private ProductRepository productRepository;
 
+    @Autowired
+    private AsyncTaskExecutor executor;
+
     //This controller handles the search request
     @GetMapping("/search")
     /*
     When using a @RequestParam("search") in a controller, we expect to extract from the request a parameter which name is equal to "search".
      */
-    public Callable<String> search(@RequestParam("search") String search, Model model, HttpServletRequest request) {
+    public DeferredResult<String> search(@RequestParam("search") String search, Model model, HttpServletRequest request) {
+        DeferredResult<String> deferredResult = new DeferredResult<>();
         System.out.println("in search controller");
         System.out.println("search criteria: " + search);
         //Here we are checking if the flag for async processing support is true.
         System.out.println("Async supported in this application: " + request.isAsyncSupported());
         System.out.println("Thread from the servlet container: " + Thread.currentThread().getName());
 
-        return () -> {
-            //This time is set to simulate the delay of a blocking call.
-            Thread.sleep(3000);
+//        return () -> {
+//            //This time is set to simulate the delay of a blocking call.
+//            Thread.sleep(3000);
+//            System.out.println("Thread from spring mvc task executor: " + Thread.currentThread().getName());
+//            List<Product> products = new ArrayList<>();
+//            products = productRepository.searchByName(search);
+//            //Here the products list retrieved from the database is assigned to a model as a key-value pair. These attributes can later be accessed on the jsp pages.
+//            model.addAttribute("products", products);
+//            return "search";
+//        };
+
+        executor.execute(() -> {
+            try {
+                Thread.sleep(6000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             System.out.println("Thread from spring mvc task executor: " + Thread.currentThread().getName());
-            List<Product> products = new ArrayList<>();
+            List<Product> products;
             products = productRepository.searchByName(search);
             //Here the products list retrieved from the database is assigned to a model as a key-value pair. These attributes can later be accessed on the jsp pages.
             model.addAttribute("products", products);
-            return "search";
-        };
+            deferredResult.setResult("search");
+        });
+        return deferredResult;
     }
 }
--- src/main/java/com/test/hplus/exceptions/ApplicationExceptionHandler.java
+++ src/main/java/com/test/hplus/exceptions/ApplicationExceptionHandler.java
@@ -2,6 +2,7 @@ package com.test.hplus.exceptions;
 
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.async.AsyncRequestTimeoutException;
 
 //@ControllerAdvice in this case handles advice of exception handling to all the controllers of the application
 @ControllerAdvice
@@ -12,7 +13,7 @@ public class ApplicationExceptionHandler {
     The exception handler in the login controller has been commented out so the exception can be directed to the global exception handler,
     but in case the exception wasn't commented out, the exception handler defined in the controller would take precedence.
     */
-    @ExceptionHandler(ApplicationException.class)
+    @ExceptionHandler({ApplicationException.class, AsyncRequestTimeoutException.class})
     public String handleException() {
         System.out.println("in global exception handler");
         return "error";
