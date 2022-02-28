2c4b584b74638aae3580edbcdf8f885d898c5f98
Gregory Szymanski <gregoryszy@gmail.com>
2022-02-28 12:33:37 -0500

    Adds a XmlViewResolver.

--- src/main/java/com/test/hplus/config/ApplicationConfig.java
+++ src/main/java/com/test/hplus/config/ApplicationConfig.java
@@ -4,6 +4,7 @@ import com.test.hplus.convertors.StringToEnumConvertor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.task.AsyncTaskExecutor;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
@@ -12,6 +13,7 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.XmlViewResolver;
 
 @Configuration
 @ComponentScan(basePackages = "com.test.hplus")
@@ -30,14 +32,14 @@ public class ApplicationConfig extends WebMvcConfigurationSupport {
     InternalResourceViewResolver is commonly used for resolving jsp files.
      The jspViewResolver method makes sure that the jsp templates are picked up from the correct folders.
      */
-    @Bean
-    public InternalResourceViewResolver jspViewResolver() {
-        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-        viewResolver.setPrefix("/WEB-INF/jsp/");
-        viewResolver.setSuffix(".jsp");
-        viewResolver.setViewClass(JstlView.class);
-        return viewResolver;
-    }
+//    @Bean
+//    public InternalResourceViewResolver jspViewResolver() {
+//        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+//        viewResolver.setPrefix("/WEB-INF/jsp/");
+//        viewResolver.setSuffix(".jsp");
+//        viewResolver.setViewClass(JstlView.class);
+//        return viewResolver;
+//    }
 
     //The FormatterRegistry parameter will allow us to register any custom convertors.
     @Override
@@ -59,5 +61,13 @@ public class ApplicationConfig extends WebMvcConfigurationSupport {
         //Sets the name of each thread.
         threadPoolTaskExecutor.setThreadNamePrefix("hplusapp-thread-");
         return threadPoolTaskExecutor;
+    }
+
+    @Bean
+    public XmlViewResolver xmlViewResolver() {
+        XmlViewResolver viewResolver = new XmlViewResolver();
+        //We have to tell the resolver about the location of the xml file.
+        viewResolver.setLocation(new ClassPathResource("views.xml"));
+        return viewResolver;
     }
 }
