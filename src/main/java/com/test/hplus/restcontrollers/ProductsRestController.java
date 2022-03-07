e744f908fd569128bc22556fe4803284f001de42
Gregory Szymanski <gregoryszy@gmail.com>
2022-03-07 00:14:32 -0500

    Adds an example of get product request using path variables and request parameters.

--- src/main/java/com/test/hplus/restcontrollers/ProductsRestController.java
+++ src/main/java/com/test/hplus/restcontrollers/ProductsRestController.java
@@ -6,10 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -37,6 +34,12 @@ public class ProductsRestController {
     public ResponseEntity getProductsByRequestParam(@RequestParam("name") String name) {
         List<Product> products = productRepository.searchByName(name);
         //ResponseEntity is an object that allows sending back information back to the client about the request.
+        return new ResponseEntity(products, HttpStatus.OK);
+    }
+    //The {name} represents a path variable.
+    @GetMapping("/hplus/rest/products/{name}")
+    public ResponseEntity getProductByPathVariable(@PathVariable("name") String name) {
+        List<Product> products = productRepository.searchByName(name);
         return new ResponseEntity(products, HttpStatus.OK);
     }
 
